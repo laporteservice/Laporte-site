c21567abeeb65dab40fd2d30cea5b08f0d1c0e9b
Douglas Faria Pereira <50893472+douglasfariap@users.noreply.github.com>
2020-11-16 07:57:48 -0300

    botoes-img

---
+++ css/js.js
@@ -0,0 +1,25 @@
+function    mudaBg1(){
+    var bg= document.getElementById("bg-top");
+    var pasta = bg.getAttribute("nome");    
+    bg.style.backgroundImage = "url(img/" + pasta +"/1.jpg)";
+
+}
+
+function    mudaBg2(){
+    var bg= document.getElementById("bg-top");
+    var pasta = bg.getAttribute("nome");    
+    bg.style.backgroundImage = "url(img/" + pasta +"/2.jpg)";
+
+}
+function    mudaBg3(){
+    var bg= document.getElementById("bg-top");
+    var pasta = bg.getAttribute("nome");    
+    bg.style.backgroundImage = "url(img/" + pasta +"/3.jpg)";
+
+}
+function    mudaBg4(){
+    var bg= document.getElementById("bg-top");
+    var pasta = bg.getAttribute("nome");    
+    bg.style.backgroundImage = "url(img/" + pasta +"/4.jpg)";
+
+}
